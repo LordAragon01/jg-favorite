66411dbe482a6f8f334310ccc9d90d0aadd0edb3
Joene <jggdevweb@gmail.com>
2021-08-27 16:27:09 +0100

    Custom Loop

--- views/jg-favorite_widget.php
+++ views/jg-favorite_widget.php
@@ -4,7 +4,9 @@
             "post_type" => "post",
             "post_per_page" => -1,
             "status" => "publish",
-            "order" => "ASC"
+            "order" => "ASC",
+            "meta_key" => "favorite_post",
+            "meta_value" => "yes"
         );
 
 
@@ -15,31 +17,23 @@
 
             while($post_loop->have_posts()):
 
-                $favorite_post = get_post_meta(get_the_ID(), 'favorite_post', true);
-
             $post_loop->the_post();
-
-             
 
           
 ?>
 
         <h3 id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
             <a href="<?php the_permalink(); ?>">
-                <?php
-
-                    if($favorite_post === "no"){    
+                <?php         
 
                     the_title(); 
-
-                    }
-                    
+                
                 ?>
             </a>
         </h3>
            
 <?php   
-
+         
             endwhile;       
 
             wp_reset_postdata();
